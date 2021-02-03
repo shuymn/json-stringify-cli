4907ecba6b24f218955cb10a4e6befc41204bf16
Shu Yamani <mail@shuymn.me>
2021-02-03 21:48:03 +0900

    implement json-stringify (#1)

---
+++ cli/cli.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"golang.org/x/xerrors"
+)
+
+type CLI struct {
+	path string
+}
+
+func New(path string) *CLI {
+	return &CLI{path: path}
+}
+
+func (c *CLI) Run() error {
+	b, err := ioutil.ReadFile(c.path)
+	if err != nil {
+		return xerrors.Errorf("%v", err)
+	}
+
+	var buf bytes.Buffer
+	if err = json.Compact(&buf, b); err != nil {
+		return xerrors.Errorf("%v", err)
+	}
+
+	b, err = json.Marshal(buf.String())
+	if err != nil {
+		return xerrors.Errorf("%v", err)
+	}
+
+	fmt.Println(string(b))
+
+	return nil
+}
