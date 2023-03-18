2b7ca9ad411e7d35b11dce9d84d3ee3a87be3b88
YAMANI Shu <mail@shuymn.me>
2023-03-18 11:30:11 +0900

    feat: change to use pkg/errors instead of xerrors

--- cli/cli.go
+++ cli/cli.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"golang.org/x/xerrors"
+	"github.com/pkg/errors"
 )
 
 type CLI struct {
@@ -20,17 +20,17 @@ func New(path string) *CLI {
 func (c *CLI) Run() error {
 	b, err := ioutil.ReadFile(c.path)
 	if err != nil {
-		return xerrors.Errorf("%v", err)
+		return errors.Errorf("%v", err)
 	}
 
 	var buf bytes.Buffer
 	if err = json.Compact(&buf, b); err != nil {
-		return xerrors.Errorf("%v", err)
+		return errors.Errorf("%v", err)
 	}
 
 	b, err = json.Marshal(buf.String())
 	if err != nil {
-		return xerrors.Errorf("%v", err)
+		return errors.Errorf("%v", err)
 	}
 
 	fmt.Print(string(b))
